c371430f1920c7fcc0d90e3b9548d5de0a33bc62
rachelannfichtner <54895844+rachelannfichtner@users.noreply.github.com>
2021-09-12 19:03:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,25 @@
-
 import praw
+import time
 
-
-previous_id="0"
-		
-reddit = praw.Reddit(    
-        user_agent="by u/Sam-Kinison-Bot",
-        client_id="hoT0dxAKni29GjA9XEJOmA",
-        client_secret="xnAqn5RemYcstRoKA4zBpTUNMmibzQ",
-        username="gaulentmaiden",
+reddit = praw.Reddit(  
+        user_agent="Sam Kinison Bot" ,
+        client_id="ubtXt_FGb831V1t4w3Os5Q",
+        client_secret="0Pf4xmkTDX5SvQ-t9b8yhBPQygHTjQ",
+        username="SamKinisonBot",
         password="WeRGr00t",)
 
-previous_id="0"
-	
-def search():
-	for results in reddit.subreddit('all').comments():	#Grab all the Recent Comments in every subreddit. This will return 100 of the newest comments on Reddit
-		global previous_id	#Import the global variable
-		
-		body = results.body  #Grab the Comment
-		body=body.lower()	#Convert the comment to lowercase so we can search it no matter how it was written
-		comment_id = results.id  #Get the Comment ID
-		
-		if comment_id == previous_id:  #Check if we already replied to this comment
-			return "Error"
-		
+subreddit = reddit.subreddit("comedy")
 
-		
-		found=body.find('. it is a joke.')	#Search for Sad. Make sure there is a period at the end of the previous sentence as well as the end of SAD
-			
-		if found != -1: 		#Looks like the comment includes . sad.
-			previous_id = comment_id  #Add to our global variable
-			
-			try:
-				results.reply('CAUSE IT IS A FUCKING JOKE OHHH OHHH OHHHHHHHHHHHHH')	#Reply to the Comment
-			except:
-				break	#Leave the function if error occurs with replying
-		
+sam_reply = ["CAUSE IT'S A JOKE OHH OHHHH OHHHHHHHHH"]
 
+for submission in subreddit.hot(limit=10):
+  #  print(submission.title)
+
+    for comment in submission.comments:
+        if hasattr(comment,"body"):
+           comment_lower = comment.body.lower()
+           if " a joke " in comment_lower:
+                # print("------")
+                 print(comment.body)
+                 comment.reply(sam_reply)
+                 time.sleep(66)
